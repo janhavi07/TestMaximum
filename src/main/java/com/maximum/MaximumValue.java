19cdbb13d386ecabd76ef79fa2d69e58c5485fe9
janhavi07 <partejanhavi@gmail.com>
2019-11-29 10:34:15 +0530

    Added code to find the max integer number.

--- src/main/java/com/maximum/MaximumValue.java
+++ src/main/java/com/maximum/MaximumValue.java
@@ -1,4 +1,21 @@
 package com.maximum;
 
 public class MaximumValue {
+    public static Integer findMaxInteger() {
+        Integer n1 = 5;
+        Integer n2 = 10;
+        Integer n3 = 2;
+        Integer max = n1;
+        if (n2.compareTo(max) > 0)
+            max = n2;
+        if (n3.compareTo(max) > 0)
+            max = n3;
+        if(n1.compareTo(max)>0)
+            max=n1;
+        return max;
+    }
+
+
+
+
 }
--- src/test/java/com/maximum/MaximumValueTest.java
+++ src/test/java/com/maximum/MaximumValueTest.java
@@ -1,4 +1,13 @@
 package com.maximum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaximumValueTest {
+    @Test
+    public void whenGivenThreeNumber_FindTheMaximum() {
+        MaximumValue maximumValue = new MaximumValue();
+        Integer maxValue=maximumValue.findMaxInteger();
+        Assert.assertEquals((Integer)10,maxValue);
+    }
 }
