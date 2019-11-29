84dadb80718931fbc1aef5c62ed55d69f3489114
janhavi07 <partejanhavi@gmail.com>
2019-11-29 11:23:26 +0530

    Refactored the code to make all the methods to a generic one

--- src/main/java/com/maximum/MaximumValue.java
+++ src/main/java/com/maximum/MaximumValue.java
@@ -1,46 +1,14 @@
 package com.maximum;
 
 public class MaximumValue {
-    public static Integer findMaxInteger() {
-        Integer n1 = 5;
-        Integer n2 = 10;
-        Integer n3 = 2;
-        Integer max = n1;
-        if (n2.compareTo(max) > 0)
-            max = n2;
-        if (n3.compareTo(max) > 0)
-            max = n3;
-        if(n1.compareTo(max)>0)
-            max=n1;
-        return max;
-    }
-
-
-    public static Float findMaxFloat() {
-        Float n1=10.52f;
-        Float n2=10.25f;
-        Float n3=10.02f;
-        Float max=n1;
-        if (n2.compareTo(max) > 0)
-            max = n2;
-        if (n3.compareTo(max) > 0)
-            max = n3;
-        if(n1.compareTo(max)>0)
-            max=n1;
-        return max;
-    }
-
-    public static String findMaxString() {
-        String string1="Janhavi";
-        String string2="Atul";
-        String string3="Parte";
-        String max=string1;
-        if (string2.compareTo(max) > 0)
-            max = string2;
-        if (string3.compareTo(max) > 0)
-            max = string3;
-        if(string1.compareTo(max)>0)
-            max=string1;
+    public <E extends Comparable<E>> E findMaximum(E a, E b,E c){
+        E max=a;
+        if(b.compareTo(max)>0)
+            max=b;
+        if(c.compareTo(max)>0)
+            max=c;
+        if(a.compareTo(max)>0)
+            max=a;
         return max;
     }
 }
--- src/test/java/com/maximum/MaximumValueTest.java
+++ src/test/java/com/maximum/MaximumValueTest.java
@@ -7,21 +7,21 @@ public class MaximumValueTest {
     @Test
     public void whenGivenThree_Integer_Number_FindTheMaximum() {
         MaximumValue maximumValue = new MaximumValue();
-        Integer maxValue=maximumValue.findMaxInteger();
+        Integer maxValue=maximumValue.findMaximum(10,5,2);
         Assert.assertEquals((Integer)10,maxValue);
     }
 
     @Test
     public void whenGivenThree_Float_Number_FindTheMaximum() {
         MaximumValue maximumValue = new MaximumValue();
-        Float maxValue=maximumValue.findMaxFloat();
+        Float maxValue=maximumValue.findMaximum(10.52f,10.25f,10.02f);
         Assert.assertEquals((Float)10.52f,maxValue);
     }
 
     @Test
     public void whenGivenThree_Strings_FindTheMaximum() {
         MaximumValue maximumValue = new MaximumValue();
-        String maxValue=maximumValue.findMaxString();
+        String maxValue=maximumValue.findMaximum("Janhavi","Atul","Parte");
         Assert.assertEquals("Parte",maxValue);
     }
 }
